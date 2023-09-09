2f58b5fa16191d5cd781bf5c24b5ea81a0cbe232
itx-shoaib <shoaibjamil43@gmail.com>
2023-09-10 01:32:45 +0500

    adding the car part in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react'
 import { Button } from './index'
+import Image from 'next/image';
 
 const Hero = () => {
   const handleClick = () => {
@@ -8,6 +9,7 @@ const Hero = () => {
   }
   return (
     <div className="hero">
+      {/* Titles div */}
       <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'>
         Your Car, Your Way: Discover, Reserve, and Roll Out!
@@ -20,6 +22,15 @@ const Hero = () => {
         componentStyle="bg-primary-blue text-white rounded-full mt-10"
         handleClick={handleClick}
         />
+      </div>
+
+      {/* Image div */}
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src="/hero.png" alt="Car_image" fill className='object-contain' />
+          <div className='hero__image-overlay' />
+        </div>
+
       </div>
     </div>
   )

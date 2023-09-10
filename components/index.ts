52061def1e5f07b3bbbf784e3148b2eeeed29f2a
itx-shoaib <shoaibjamil43@gmail.com>
2023-09-10 21:57:41 +0500

    making the footer and constants

--- components/index.ts
+++ components/index.ts
@@ -2,11 +2,13 @@
 import Hero from "./Hero";
 import Button from "./Button";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 
 // exporting all components here
 export {
     Hero,
     Button,
-    Navbar
+    Navbar,
+    Footer
 }
